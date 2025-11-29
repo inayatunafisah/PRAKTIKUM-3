5e9f01d26a110d607d0463b231fab4728648af7f
inay <inayatunafisah@gmail.com>
2025-11-29 14:28:28 +0700

    Add files via upload

---
+++ PRAKTIKUM 3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 7;
+
+    cout << "Masukkan jumlah baris segitiga pascal : ";
+    cin >> n;
+
+
+    for (int i = 0; i < n; i++) {
+
+
+        for (int s = 0; s < n - i; s++) {
+            cout << " ";
+        }
+
+        int val = 1;
+
+        for (int j = 0; j <= i; j++) {
+            cout << val << " ";
+
+            val = val * (i - j) / (j + 1);
+        }
+        
+        cout << endl;
+    }
+
+    return 0;
+}
